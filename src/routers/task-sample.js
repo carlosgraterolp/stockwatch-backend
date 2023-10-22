a6a039af86c65d0902804a8e8cfe4984eece3922
Carlos Graterol <carlos.graterol@icloud.com>
2023-10-22 13:04:59 -0400

    Add 404, refactor Utils and format code

--- src/routers/task-sample.js
+++ src/routers/task-sample.js
@@ -6,7 +6,7 @@ const router = new express.Router()
 router.post('/tasks', auth, async (req, res) => {
     const task = new Task({
         ...req.body,
-        owner: req.user._id
+        owner: req.user._id,
     })
 
     try {
@@ -45,20 +45,25 @@ router.get('/tasks/:id', auth, async (req, res) => {
 router.patch('/tasks/:id', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+    const isValidOperation = updates.every((update) =>
+        allowedUpdates.includes(update)
+    )
 
     if (!isValidOperation) {
         return res.status(400).send({ error: 'Invalid updates!' })
     }
 
     try {
-        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id})
+        const task = await Task.findOne({
+            _id: req.params.id,
+            owner: req.user._id,
+        })
 
         if (!task) {
             return res.status(404).send()
         }
 
-        updates.forEach((update) => task[update] = req.body[update])
+        updates.forEach((update) => (task[update] = req.body[update]))
         await task.save()
         res.send(task)
     } catch (e) {
@@ -68,7 +73,10 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 
 router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
+        const task = await Task.findOneAndDelete({
+            _id: req.params.id,
+            owner: req.user._id,
+        })
 
         if (!task) {
             res.status(404).send()
---
+++ src/utils.js
@@ -0,0 +1,16 @@
+let utils = {}
+
+utils.getTodayString = () => {
+    let ts = Date.now()
+    let date_ob = new Date(ts)
+
+    return (
+        date_ob.getFullYear() +
+        '-' +
+        (date_ob.getMonth() + 1) +
+        '-' +
+        date_ob.getDate()
+    )
+}
+
+module.exports = utils
